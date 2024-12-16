266fa97c06e87c9532464867c203628a308f2a9e
achraf12-ux <achrafmoukili310@gmail.com>
2024-12-16 10:11:42 +0100

    2eme commit

--- src/main/java/convertion/ArabicRomanNumerals.java
+++ src/main/java/convertion/ArabicRomanNumerals.java
@@ -5,8 +5,8 @@ public class ArabicRomanNumerals {
    public static String convert(int nbr){
        if(nbr==1) return "I";
 
-     /* else if (nbr==2) return "II";
-      else if (nbr==3) return "III";
+     else if (nbr==2) return "II";
+      /*else if (nbr==3) return "III";
       else */
       else return null;
 
